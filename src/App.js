c242b6bb76e40150b02b4e33448ce4dc75955491
Oliver Ziemann <oliver@mordoor.de>
2021-12-17 14:17:23 +0100

    change nav item titles

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="bg-tertiary flex flex-grow">
       <div className=" flex flex-col flex-grow items-center m-10">
         <nav className="w-90p h-20 bg-neutral shadow-xl rounded-lg flex flex-row justify-start items-center">
-          <Link to="/invoices" className="ml-8 mr-4">Invoices</Link>
-          <Link to="/expenses" className="mx-4">Expenses</Link>
+          <Link to="/invoices" className="ml-8 mr-4">Location Picker</Link>
+          <Link to="/expenses" className="mx-4">Item Cards</Link>
         </nav>
         <div className="w-90p flex-grow flex justify-end items-end">
           <MainContentCard></MainContentCard>
